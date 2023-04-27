fb5132a556c5ac08d9daa9bb22385455a5e720bc
Chick Chicky <91527355+ChickChicky@users.noreply.github.com>
2023-04-27 14:50:30 +0200

    Add files via upload

--- nuclearStatus.lua
+++ nuclearStatus.lua
@@ -8,7 +8,8 @@ while true do
 
     if username == "Chick_Chicky" and message == "plz reactor status" then
         
-        local radiation = environment.getRadiation().radiation
+        local rad = environment.getRadiation()
+        local radiation = rad.radiation..rad.unit
         local transferRate = energy.getTransferRate()
 
         chatBox.sendMessage('Radiation: §a'..tostring(radiation)..'§r\nEnergy Transfer: §e'..tostring(transferRate)..'§r','NuclearMonitor','[]')
